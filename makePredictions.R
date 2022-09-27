5f5a3d01a70a58921ea914ae051510808caf7b39
Thomas Sigall <thomas@sigall.org>
2022-09-27 11:58:27 -0400

    Round numbers in prediction table

--- makePredictions.R
+++ makePredictions.R
@@ -2,12 +2,12 @@
 makePred <- function(Season, Week){
   game <- subset(modelData, season == Season & week == Week)
   weekPreds <- predict(model, game[])
-  weekResults<-data.frame(game[,1:4], weekPreds, game[,6])
-  names(weekResults)<-c('Season', 'Week', 'Away', 'Home', 'Predicted', 'OU')
-  weekResults<-transform(weekResults,'Difference'= ifelse(Predicted>0, Predicted-OU, 0))
+  weekResults<-data.frame(game[,1:4], round(weekPreds, 2), game[,6])
+  names(weekResults)<-c('Season', 'Week', 'Away', 'Home', 'Pred.', 'OU')
+  weekResults<-transform(weekResults,'Diff.'= ifelse(Pred.>0, Pred.-OU, 0))
   weekResults<-transform(weekResults,'Bet' = if_else(weekResults[,7] > 0, 'Over', 'Under', 'No data'))
   weekResults
 }
 
 
-makePred(2022, 3)
+makePred(2022, 4)
